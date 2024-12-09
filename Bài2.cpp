5b6ead5691eae465489ca717e142abfc68ae9e93
HoangDuong20-06 <HoangDuong2062006@gmail.com>
2024-12-09 20:46:58 +0700

    Add files via upload

---
+++ Bài2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int ham(int a, int b, int  c, int d, int e){
+	printf("%d ",a);
+	printf("%d ",b);
+	printf("%d ",c);
+	printf("%d ",d);
+	printf("%d ",e);
+	return a,b,c,d,e; 
+	}
+int main(){
+	ham(1,5,9,6,0);
+}
